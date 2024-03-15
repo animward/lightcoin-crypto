6f4606f63656c912b35c02178fe58c1cdb6fb2e1
animward <animward@gmail.com>
2024-03-15 12:43:53 -0700

    deposit function

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 let balance = 500.00;
+console.log('Starting Balance:', balance); 
+
+class Deposit {
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  commit(account) {
+    account.balance += this.amount;
+  }
+};
 
 class Withdrawal {
 
@@ -10,7 +21,7 @@ class Withdrawal {
     balance -= this.amount;
   }
 
-}
+};
 
 const account = { balance: balance };
 var t1 = new Withdrawal(50.25);
@@ -20,6 +31,10 @@ console.log('Transaction 1:', t1);
 var t2 = new Withdrawal(9.99);
 t2.commit(account);
 console.log('Transaction 2:', t2);
+
+var t3 = new Deposit(120.00);
+t3.commit(account);
+console.log('Transaction 3:', t3);
 
 console.log('Balance:', balance);
 
